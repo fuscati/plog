9ccae4efdc5473a3b8a79c39ac09d34e157d713d
Joao Rocha <jr007silva1306@gmail.com>
2020-11-29 23:33:41 +0000

    Added Player vs PC loop

--- proj1/play.pl
+++ proj1/play.pl
@@ -1,12 +1,21 @@
-startGame(_Player1, _Player2) :-
+startGame('P', 'P') :-
     initial(GameState),
     play_loop(GameState,-1,8,8),
+    nl,
+    display_winner(Winner).
+
+startGame('P', 'C') :-
+    initial(GameState),
+    play_loop2(GameState,-1,8,8),
     nl,
     display_winner(Winner).
 
 %há vencedor e acaba o loop
 play_loop(_,1,_,_).
 play_loop(_,2,_,_).
+
+play_loop2(_,1,_,_).
+play_loop2(_,2,_,_).
 
 %define o vencedor
 winner_white(GameState,Winner):-
@@ -24,10 +33,16 @@ winner_black(_,-1).
 
 %loop do jogo, faz uma jogada do preto e uma do branco alternadamente, verificando a condição de vitória
 play_loop(GameState,Winner,WhiteRings,BlackRings) :-
-
   game_white(GameState,Winner,WhiteRings,NewWhiteGameState,NewWhiteRings),
   winner_white(NewWhiteGameState,Winner),!,
   game_black(NewWhiteGameState,Winner,BlackRings,NewBlackGameState,NewBlackRings),
+  winner_black(NewBlackGameState,Winner),!,
+  play_loop(NewBlackGameState,Winner,NewWhiteRings,NewBlackRings).
+
+play_loop2(GameState,Winner,WhiteRings,BlackRings) :-
+  %game_white(GameState,Winner,WhiteRings,NewWhiteGameState,NewWhiteRings),
+  %winner_white(NewWhiteGameState,Winner),!,
+  game_bot(GameState,Winner,BlackRings,NewBlackGameState,NewBlackRings),
   winner_black(NewBlackGameState,Winner),!,
   play_loop(NewBlackGameState,Winner,NewWhiteRings,NewBlackRings).
 
@@ -74,6 +89,17 @@ option(2,GameState,Player,Rings,NewRings,NewGameState):-
     NewRings is Rings,
     option(1,NGameState,Player,8,_,NewGameState).
 
+option_bot(1,GameState,Player,Rings,NewRings,NewGameState):-
+    get_add_ring_possibilities(GameState,Rings,Possibilities),
+    (call(check_possibilities(Possibilities)) -> true, !; fail),
+    get_easy_bot_decision_rings(Possibilities,Column,NRow),
+    check_add_ring(GameState,NRow,Column,Rings,Bool),
+    add_ring(GameState,Player,NRow,Column,Rings,NewRings,NewGameState).
+
+get_easy_bot_decision_rings([[Column,Row]|_],Column,Row).
+
+get_easy_bot_decision_ball([[[ColumnFrom,RowFrom],[ColumnTo,RowTo]]|_],ColumnFrom,RowFrom,ColumnTo,RowTo).
+
 %descobre todas as jogadas possíveis de remover anéis para depois os mover
 get_remove_ring_possibilities(GameState,Player,Possibilities) :-
     findall([Column,Row], check_remove_ring(Player,GameState,Row,Column,true), Possibilities),
@@ -106,6 +132,15 @@ read_move_ball(GameState,Player,NGameState):-
     move_ball(GameState,Row_from,Column_from,Row_to,Column_to,NewGameState,Player),
     display_game(NewGameState,Player,Rings),
     vault(NewGameState,Row_from,Column_from,Row_to,Column_to,NGameState,Player,Vault).
+
+read_move_ball_bot(GameState,Player,ColumnFrom,RowFrom,ColumnTo,RowTo,NGameState):-
+    check_ball_from_move(Player,GameState,RowFrom,ColumnFrom),
+    check_ball_to_move(Player,GameState,RowTo,ColumnTo),
+    can_move(GameState,Player,RowFrom,ColumnFrom,RowTo,ColumnTo,Bool,Vault),
+    repeat_can_move(GameState,Player,RowFrom,ColumnFrom,ColumnTo,RowTo,Bool,Vault),
+    move_ball(GameState,RowFrom,ColumnFrom,RowTo,ColumnTo,NewGameState,Player),
+    display_game(NewGameState,Player,Rings),
+    vault(NewGameState,RowFrom,ColumnFrom,RowTo,ColumnTo,NGameState,Player,Vault).
 
 repeat_can_move(_GameState,_Player,_Row_from,_Column_from, _Column_to, _Row_to,Bool,_Vault):-
 Bool>0.
@@ -149,3 +184,17 @@ game_black(GameState,X,Rings_black,NewGameState,NewRings):-
   (call(option(NewOption,GameState,'black',Rings_black,NewRings,NGameState)) -> true, !; fail),
   display_game(NGameState,'black',NewRings),
   read_move_ball(NGameState,'black',NewGameState).
+
+game_bot(GameState,X,Rings_black,NewGameState,NewRings) :-
+  display_game(GameState,'black',Rings_black),
+  nl,
+  write('Player black'),
+  option_bot(1,GameState,'black',Rings_black,NewRings,NGameState),
+  display_game(NGameState,'black',NewRings).
+  %read_move_ball(NGameState,'black',NewGameState).
+  %get_move_ball_possibilities(NGameState,'black',Possibilities),
+  %nl,
+  %write(Possibilities),
+  %get_easy_bot_decision_ball(Possibilities,ColumnFrom,RowFrom,ColumnTo,RowTo),
+  %format('\nCFrom: ~w RFrom: ~w CTo: ~w RTo: ~w', [ColumnFrom,RowFrom,ColumnTo,RowTo]).
+  %read_move_ball_bot(NGameState,'black',ColumnFrom,RowFrom,ColumnTo,RowTo,NewGameState).
